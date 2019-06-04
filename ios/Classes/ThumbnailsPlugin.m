dee3ad567c543227c4fb36627f8e509f5698e08e
aaverty <aaverty@student.42.fr>
2019-06-04 17:17:20 +0200

    add ios implementation

--- ios/Classes/ThumbnailsPlugin.m
+++ ios/Classes/ThumbnailsPlugin.m
@@ -1,4 +1,6 @@
 #import "ThumbnailsPlugin.h"
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation ThumbnailsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -12,6 +14,38 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if ([@"getThumbnail" isEqualToString:call.method]) {
+      @try {
+          NSString *filepath = call.arguments[@"videoFilePath"];
+          filepath = [filepath stringByReplacingOccurrencesOfString:@"file://"
+                                                         withString:@""];
+          NSURL *videoURL = [NSURL fileURLWithPath:filepath];
+          
+          MPMoviePlayerController *moviePlayer = [[MPMoviePlayerController alloc]
+                                                  initWithContentURL:videoURL];
+          moviePlayer.shouldAutoplay = NO;
+          UIImage *thumbnail = [moviePlayer thumbnailImageAtTime:1.0
+                                                      timeOption:MPMovieTimeOptionNearestKeyFrame];
+          // save to document directory
+          NSString* documentDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                                         NSUserDomainMask,
+                                                                         YES) lastObject];
+          if (thumbnail != nil) {
+              NSData *data = UIImageJPEGRepresentation(thumbnail, 1.0);
+              NSFileManager *fileManager = [NSFileManager defaultManager];
+              NSString *fullPath = [documentDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"thumb-%@.jpg", [[NSProcessInfo processInfo] globallyUniqueString]]];
+              bool fileIsWrited = [fileManager createFileAtPath:fullPath contents:data attributes:nil];
+              if (result && fileIsWrited) {
+                  result(fullPath);
+              } else {
+                  result(@"An error occurs when try to write the output image file");
+              }
+          } else {
+              result(@"An error occurs when try to extract image from video");
+          }
+      } @catch(NSException *e) {
+          result(@{@"error": e.reason});
+      }
   } else {
     result(FlutterMethodNotImplemented);
   }
